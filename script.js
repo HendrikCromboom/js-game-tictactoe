78920f5c5561bfb06c5cca59c790902d3404dba9
HendrikCromboom <hcromboom@gmail.com>
2020-09-10 02:53:25 +0200

    added more ux

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var arrOptions = ["Rock", "Paper", "Scissor"];
+var oppOptions = ["oppRock", "oppPaper", "oppScissor"];
 function getId(par) { return document.getElementById(par); }
 function listenPar(par, par2, par3) { return getId(par).addEventListener(par2, par3); }
 function inner(par, par4) { return (getId(par).innerHTML = par4); }
@@ -7,48 +8,17 @@ function youDraw() { (inner("choice", "It's a draw")); }
 function youLose() { inner("choice", "You Lose..."); }
 arrOptions.forEach(function (option, i) {
     listenPar(arrOptions[i], "click", function () {
+        for (var b = 0; b < oppOptions.length; b++) {
+            getId(oppOptions[b]).style.color = "white";
+            getId(arrOptions[b]).style.color = "white";
+        }
+        getId(arrOptions[i]).style.color = "green";
         var playerValue = i;
         var opponentValue = (Math.floor(Math.random() * 3));
-        switch (playerValue) {
-            case (0):
-                switch (opponentValue) {
-                    case (0):
-                        youDraw();
-                        break;
-                    case (1):
-                        youLose();
-                        break;
-                    case (2):
-                        youWin();
-                        break;
-                }
-                break;
-            case (1):
-                switch (opponentValue) {
-                    case (0):
-                        youWin();
-                        break;
-                    case (1):
-                        youDraw();
-                        break;
-                    case (2):
-                        youLose();
-                        break;
-                }
-                break;
-            case (2):
-                switch (opponentValue) {
-                    case (0):
-                        youLose();
-                        break;
-                    case (1):
-                        youWin();
-                        break;
-                    case (2):
-                        youDraw();
-                        break;
-                }
-                break;
-        }
+        getId(oppOptions[opponentValue]).style.color = "red";
+        function checking() { playerValue === opponentValue ? youDraw() : (playerValue === opponentValue - 1 ? youLose() : (playerValue === opponentValue + 1 ? youWin() : (playerValue === opponentValue - 2 ? youWin() : youLose()))); }
+        console.log(checking());
+        console.log("You picked " + arrOptions[playerValue]);
+        console.log("Opponent picked " + arrOptions[opponentValue]);
     });
 });
--- script.ts
+++ script.ts
@@ -1,34 +1,23 @@
 const arrOptions: string[] = ["Rock", "Paper", "Scissor"]
+const oppOptions: string[] = ["oppRock", "oppPaper", "oppScissor"]
 function getId(par:string){ return document.getElementById(par)}
 function listenPar(par:string,par2:string,par3:any){ return getId(par).addEventListener(par2, par3)}
 function inner(par:string, par4:string){ return (getId(par).innerHTML= par4) }
-function youWin() {inner("choice", "You Win !")}
+function youWin() {inner("choice", "You Win !");}
 function youDraw() {(inner("choice", "It's a draw"))}
 function youLose() {inner("choice", "You Lose...")}
 arrOptions.forEach((option:string, i:number)=>{
     listenPar(arrOptions[i], "click", ()=>{
+        for(var b = 0; b < oppOptions.length; b++ ){getId(oppOptions[b]).style.color = "white";getId(arrOptions[b]).style.color = "white";}
+        getId(arrOptions[i]).style.color = "green";
         let playerValue = i
         let opponentValue = (Math.floor(Math.random() * 3))
-        switch (playerValue){
-            case(0):
-                switch(opponentValue){
-                    case(0):youDraw(); break;
-                    case(1):youLose(); break;
-                    case(2):youWin();break;
-                    }break;
-            case(1):
-                switch(opponentValue){
-                    case(0):youWin(); break;
-                    case(1):youDraw(); break;
-                    case(2):youLose();break;
-                }break;
-            case(2):
-                switch(opponentValue){
-                    case(0):youLose(); break;
-                    case(1):youWin(); break;
-                    case(2):youDraw();break;
-                }break;
-        }
+        getId(oppOptions[opponentValue]).style.color = "red";
+        function checking(){playerValue === opponentValue ? youDraw():(playerValue === opponentValue-1 ? youLose():(playerValue===opponentValue+1 ? youWin():(playerValue===opponentValue-2? youWin():youLose())))}
+        console.log(checking())
+        console.log("You picked " + arrOptions[playerValue])
+        console.log("Opponent picked " + arrOptions[opponentValue])
+
     })
 })
 
