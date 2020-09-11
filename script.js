09d767ff047292fcc022f8c4242119fa8489d87e
HENDRIK <hendrik@KALI.home>
2020-09-11 15:51:15 +0200

    added function calls and pseudo constructors

--- script.js
+++ script.js
@@ -2,12 +2,16 @@ var arrOptions = ["Rock", "Paper", "Scissor", "Spock", "Lizard"];
 var oppOptions = ["oppRock", "oppPaper", "oppScissor", "oppSpock", "oppLizard"];
 var yourScore = 0;
 var oppoScore = 0;
-function getId(par) { return document.getElementById(par); }
-function listenPar(par, par2, par3) { return getId(par).addEventListener(par2, par3); }
-function inner(par, par4) { return (getId(par).innerHTML = par4); }
-function youWin() { inner("choice", "You Win !"); yourScore++; inner("youScore", "Score: " + yourScore); }
-function youDraw() { (inner("choice", "It's a draw")); }
-function youLose() { inner("choice", "You Lose..."); oppoScore++; inner("oppScore", "Score: " + oppoScore); }
+function getId(id) { return document.getElementById(id); }
+function listenPar(id, event, func) { return getId(id).addEventListener(event, func); }
+function inner(id, innerHTML) { return (getId(id).innerHTML = innerHTML); }
+function youWinMessage() { inner("choice", "You Win !"); }
+function youDraw() { inner("choice", "It's a draw"); }
+function youLoseMesssage() { inner("choice", "You Lose..."); }
+function youScore() { yourScore++; inner("youScore", "Score: " + yourScore); }
+function oppScore() { oppoScore++; inner("oppScore", "Score: " + oppoScore); }
+var youWin;
+var youLose;
 arrOptions.forEach(function (option, i) {
     listenPar(arrOptions[i], "click", function () {
         for (var b = 0; b < oppOptions.length; b++) {
@@ -20,14 +24,14 @@ arrOptions.forEach(function (option, i) {
         getId(oppOptions[opponentValue]).style.borderColor = "red";
         function checking() {
             playerValue === opponentValue ? youDraw() :
-                (playerValue === opponentValue - 1 ? youLose() :
-                    (playerValue === opponentValue + 1 ? youWin() :
-                        (playerValue === opponentValue - 4 ? youWin() :
-                            (playerValue === opponentValue + 4 ? youLose() :
-                                (playerValue === opponentValue - 2 ? youWin() :
-                                    (playerValue === opponentValue + 2 ? youLose() :
-                                        (playerValue === opponentValue + 3 ? youWin() :
-                                            youLose())))))));
+                (playerValue === opponentValue - 1 ? youLose(youLoseMesssage(), oppScore()) :
+                    (playerValue === opponentValue + 1 ? youWin(youWinMessage(), youScore()) :
+                        (playerValue === opponentValue - 4 ? youWin(youWinMessage(), youScore()) :
+                            (playerValue === opponentValue + 4 ? youLose(youLoseMesssage(), oppScore()) :
+                                (playerValue === opponentValue - 2 ? youWin(youWinMessage(), youScore()) :
+                                    (playerValue === opponentValue + 2 ? youLose(youLoseMesssage(), oppScore()) :
+                                        (playerValue === opponentValue + 3 ? youWin(youWinMessage(), youScore()) :
+                                            youLose(youLoseMesssage(), oppScore()))))))));
         }
         console.log(checking());
         console.log("You picked " + arrOptions[playerValue]);
--- script.ts
+++ script.ts
@@ -2,29 +2,40 @@ const arrOptions: string[] = ["Rock", "Paper", "Scissor", "Spock", "Lizard"]
 const oppOptions: string[] = ["oppRock", "oppPaper", "oppScissor", "oppSpock", "oppLizard"]
 let yourScore:number = 0
 let oppoScore:number = 0
-function getId(par:string){ return document.getElementById(par)}
-function listenPar(par:string,par2:string,par3:any){ return getId(par).addEventListener(par2, par3)}
-function inner(par:string, par4:string){ return (getId(par).innerHTML= par4) }
-function youWin() {inner("choice", "You Win !");yourScore ++;inner("youScore", "Score: " + yourScore )}
-function youDraw() {(inner("choice", "It's a draw"))}
-function youLose() {inner("choice", "You Lose..."); oppoScore++; inner("oppScore", "Score: " + oppoScore)}
-arrOptions.forEach((option:string, i:number)=>{
+function getId(id:string):HTMLElement{ return document.getElementById(id)}
+function listenPar(id:string,event:string,func:any):void{ return getId(id).addEventListener(event, func)}
+function inner(id:string, innerHTML:string):string{ return (getId(id).innerHTML= innerHTML) }
+function youWinMessage():void {inner("choice", "You Win !")}
+function youDraw():void {inner("choice", "It's a draw")}
+function youLoseMesssage():void {inner("choice", "You Lose...")}
+function youScore(){yourScore ++;inner("youScore", "Score: " + yourScore)}
+function oppScore(){oppoScore++; inner("oppScore", "Score: " + oppoScore)}
+let youWin: (a:void, b:void) => Function;
+let youLose: (a:void, b:void) => Function;
+arrOptions.forEach((option:string, i:number):void=>{
     listenPar(arrOptions[i], "click", ()=>{
-        for(var b = 0; b < oppOptions.length; b++ ){getId(oppOptions[b]).style.borderColor = "white";getId(arrOptions[b]).style.borderColor = "white";}
+        for(let b = 0; b < oppOptions.length; b++ ){getId(oppOptions[b]).style.borderColor = "white";getId(arrOptions[b]).style.borderColor = "white";}
         getId(arrOptions[i]).style.borderColor = "green";
-        let playerValue = i
-        let opponentValue = (Math.floor(Math.random() * 5))
+        let playerValue:number = i
+        let opponentValue:number = (Math.floor(Math.random() * 5))
         getId(oppOptions[opponentValue]).style.borderColor = "red";
-        function checking(){playerValue === opponentValue ? youDraw():
-            (playerValue === opponentValue-1 ? youLose():
-                (playerValue===opponentValue+1 ? youWin():
-                    (playerValue===opponentValue-4? youWin():
-                        (playerValue===opponentValue+4? youLose():
-                            (playerValue === opponentValue-2 ? youWin():
-                                (playerValue===opponentValue+2 ? youLose():
-                                    (playerValue===opponentValue+3 ? youWin():
-                                    youLose()
-                                    )))))))}
+        function checking():void{playerValue === opponentValue ? youDraw():
+            (playerValue === opponentValue-1 ? youLose(youLoseMesssage(), oppScore()):
+                (playerValue===opponentValue+1 ? youWin(youWinMessage(), youScore()):
+                    (playerValue===opponentValue-4? youWin(youWinMessage(), youScore()):
+                        (playerValue===opponentValue+4? youLose(youLoseMesssage(), oppScore()):
+                            (playerValue === opponentValue-2 ? youWin(youWinMessage(), youScore()):
+                                (playerValue===opponentValue+2 ? youLose(youLoseMesssage(), oppScore()):
+                                    (playerValue===opponentValue+3 ? youWin(youWinMessage(), youScore()):
+                                            youLose(youLoseMesssage(), oppScore())
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        }
         console.log(checking())
         console.log("You picked " + arrOptions[playerValue])
         console.log("Opponent picked " + arrOptions[opponentValue])
