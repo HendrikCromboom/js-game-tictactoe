beec577273b713e58773fc02b68626d4a525470c
HENDRIK <hendrik@KALI.home>
2020-09-11 10:34:55 +0200

    added 2 more options

--- script.ts
+++ script.ts
@@ -1,5 +1,5 @@
-const arrOptions: string[] = ["Rock", "Paper", "Scissor"]
-const oppOptions: string[] = ["oppRock", "oppPaper", "oppScissor"]
+const arrOptions: string[] = ["Rock", "Paper", "Scissor", "Spock", "Lizard"]
+const oppOptions: string[] = ["oppRock", "oppPaper", "oppScissor", "oppSpock", "oppLizard"]
 function getId(par:string){ return document.getElementById(par)}
 function listenPar(par:string,par2:string,par3:any){ return getId(par).addEventListener(par2, par3)}
 function inner(par:string, par4:string){ return (getId(par).innerHTML= par4) }
@@ -11,13 +11,21 @@ arrOptions.forEach((option:string, i:number)=>{
         for(var b = 0; b < oppOptions.length; b++ ){getId(oppOptions[b]).style.borderColor = "white";getId(arrOptions[b]).style.borderColor = "white";}
         getId(arrOptions[i]).style.borderColor = "green";
         let playerValue = i
-        let opponentValue = (Math.floor(Math.random() * 3))
+        let opponentValue = (Math.floor(Math.random() * 5))
         getId(oppOptions[opponentValue]).style.borderColor = "red";
-        function checking(){playerValue === opponentValue ? youDraw():(playerValue === opponentValue-1 ? youLose():(playerValue===opponentValue+1 ? youWin():(playerValue===opponentValue-2? youWin():youLose())))}
+        function checking(){playerValue === opponentValue ? youDraw():
+            (playerValue === opponentValue-1 ? youLose():
+                (playerValue===opponentValue+1 ? youWin():
+                    (playerValue===opponentValue-4? youWin():
+                        (playerValue===opponentValue+4? youLose():
+                            (playerValue === opponentValue-2 ? youWin():
+                                (playerValue===opponentValue+2 ? youLose():
+                                    (playerValue===opponentValue+3 ? youWin():
+                                    youLose()
+                                    )))))))}
         console.log(checking())
         console.log("You picked " + arrOptions[playerValue])
         console.log("Opponent picked " + arrOptions[opponentValue])
-
     })
 })
 
